551a34adbef3fae01cdd063636e4b007f8180c58
hyorim-e <hyorim1033@gmail.com>
2022-08-19 23:26:40 +0900

    터치 시 공 생성, 바닥 토글 활성화

--- Assets/Scripts/ARManager.cs
+++ Assets/Scripts/ARManager.cs
@@ -6,6 +6,29 @@ using UnityEngine.XR.ARSubsystems;
 
 public class ARManager : MonoBehaviour
 {
+    #region 바닥에 프리팹 놓기
+
+    public ARRaycastManager arRaycaster;
+    public GameObject spawnPrefab;
+    List<ARRaycastHit> hits = new List<ARRaycastHit>();
+
+    void PlacePrefab()
+    {
+        if (Input.touchCount == 0) return;
+
+        Touch touch = Input.GetTouch(0);
+        if (touch.phase != TouchPhase.Began) return;
+
+        if(arRaycaster.Raycast(touch.position, hits, TrackableType.Planes))
+        {
+            Pose hitPose = hits[0].pose;
+            Instantiate(spawnPrefab, hitPose.position, hitPose.rotation);
+        }
+    }
+
+    #endregion
+
+
     #region 플레이어를 중심으로 이동
 
     public GameObject TouchParticle;
@@ -21,14 +44,22 @@ public class ARManager : MonoBehaviour
 
     #endregion
 
-    void Start()
-    {
-        
-    }
 
     void Update()
     {
-        
+        PlacePrefab();
     }
 
+
+    #region 바닥 활성화
+
+    public ARPlaneManager arPlane;
+
+    public void ShowPlane(bool b)
+    {
+        foreach (var plane in arPlane.trackables)
+            plane.gameObject.SetActive(b);
+    }
+
+    #endregion
 }
