2d4bfd624d9fdde4bb9c95ab75a9527770ae705b
hyorim-e <hyorim1033@gmail.com>
2022-08-21 23:39:55 +0900

    일정 거리 내 물체만 보이도록 스크립트 작성

---
+++ Assets/Scripts/HideObject.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HideObject : MonoBehaviour
+{
+    WaitForSeconds Delay04 = new WaitForSeconds(0.4f);
+    Renderer RR;
+    void Start()
+    {
+        RR = GetComponent<Renderer>();
+        StartCoroutine(HideRendererCo());
+    }
+
+IEnumerator HideRendererCo()
+    {
+        while (true)
+        {
+            RR.enabled = (PlayerPos.playerPos - transform.position).sqrMagnitude < 2116;
+            yield return Delay04;
+        }
+    }
+}
