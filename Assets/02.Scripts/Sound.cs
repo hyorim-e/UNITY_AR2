52cd79b186390e32bca693febf5374e3e6f0aa8b
sunghwi0324 <108246323+sunghwi0324@users.noreply.github.com>
2023-02-13 14:14:43 +0900

    동물 눈동자, start 버튼 효과음

--- Assets/02.Scripts/Sound.cs
+++ Assets/02.Scripts/Sound.cs
@@ -6,11 +6,13 @@ public class Sound : MonoBehaviour
 {
     public AudioSource footstepsSound;
     public AudioSource buttonSound;
+    public AudioSource startbuttonSound;
 
     void Start()
     {
         footstepsSound = GetComponent<AudioSource>();
         buttonSound = GetComponent<AudioSource>();
+        startbuttonSound = GetComponent<AudioSource>();
     }
 
     public void footstepSound()
@@ -23,6 +25,10 @@ public class Sound : MonoBehaviour
         buttonSound.Play();
     }
 
+    public void startbuttonClickSound()
+    {
+        startbuttonSound.Play();
+    }
 }
 
 
