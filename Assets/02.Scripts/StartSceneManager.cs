36d74bcbdc89e87292401fc5a289e247ca8b65b3
Hyorim Choi <hyorim1033@gmail.com>
2023-10-02 20:12:45 +0900

    스타트씬 로딩 추가

--- Assets/02.Scripts/StartSceneManager.cs
+++ Assets/02.Scripts/StartSceneManager.cs
@@ -1,10 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using UnityEngine.SceneManagement;
 
 public class StartSceneManager : MonoBehaviour
 {
+    public int loadSceneIndex;
+
+    public Slider progressSlider;
+    public GameObject startBtn;
+    public GameObject progressObj;
+
+    private void Start()
+    {
+        StartCoroutine(LoadScene_Coroutine(loadSceneIndex));
+    }
+
+    public IEnumerator LoadScene_Coroutine(int index)
+    {
+        progressSlider.value = 0;
+
+        AsyncOperation asyncOperation = SceneManager.LoadSceneAsync(index);
+        asyncOperation.allowSceneActivation = false;
+        float progress = 0;
+
+        while (!asyncOperation.isDone)
+        {
+            progress = Mathf.MoveTowards(progress, asyncOperation.progress, Time.deltaTime);
+            progressSlider.value = progress;
+            if (progress >= 0.9f)
+            {
+                progressSlider.value = 1;
+                startBtn.SetActive(true);
+                progressObj.SetActive(false);
+            }
+            yield return null;
+        }
+    }
+
     public void OnClickButton()
     {
         SceneManager.LoadScene("MergeScene");
